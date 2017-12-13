20a8e0a27235fbd8dbb3855996682446d522d579
Guan Bo <guanbo2002@gmail.com>
2017-12-13 18:59:02 +0800

    Add: S3 PutObject triger

--- lambda/index.js
+++ lambda/index.js
@@ -6,26 +6,46 @@ const S3 = new AWS.S3({
 });
 const Sharp = require('sharp');
 
-const BUCKET = process.env.BUCKET;
-const URL = process.env.URL;
+const RESIZE_BASE_SIZE = 1024*1024*2;
+
+let BUCKET = process.env.BUCKET||'resize-test';
+const URL = process.env.URL||' http://resize-test.s3-website.cn-north-1.amazonaws.com.cn/';
 
 exports.handler = function(event, context, callback) {
-  const key = event.queryStringParameters.key;
-  const match = key.match(/(\d+)x(\d+)\/(.*)/);
-  const width = parseInt(match[1], 10);
-  const height = parseInt(match[2], 10);
-  const originalKey = match[3];
+  let key, match, originalKey;
+  let width = 1024;
+  let height = 1024;
+  if (event.queryStringParameters) {
+    key = event.queryStringParameters.key;
+    match = key.match(/(\d+)x(\d+)\/(.*)/);
+    width = parseInt(match[1], 10);
+    height = parseInt(match[2], 10);
+    originalKey = match[3];
+  } else if (event.Records && event.Records.length>0) {
+    let item = event.Records[0];
+    if (item.s3.object.size < RESIZE_BASE_SIZE) {
+      return callback(null);
+    }
+    BUCKET = item.s3.bucket.name;
+    originalKey = item.s3.object.key
+    match = originalKey.match(/(\d+)x(\d+)\/(.*)/);
+    if (match && match.length > 2) {
+      return callback(null)
+    }
+    key = width+'x'+height+'/'+originalKey;
+  }
 
   S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
     .then(data => Sharp(data.Body)
       .resize(width, height)
-      .toFormat('png')
+      .max()
+      .jpeg()
       .toBuffer()
     )
     .then(buffer => S3.putObject({
         Body: buffer,
         Bucket: BUCKET,
-        ContentType: 'image/png',
+        ContentType: 'image/jpg',
         Key: key,
       }).promise()
     )
